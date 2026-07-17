566ad03d093756920de0b68ff292c2fec31591a6
Hieu <hieumochd1@gmail.com>
2026-07-17 19:10:30 +0700

    Hoan thanh Lab 5 - Angular HttpClient GET API

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,32 +1,17 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { About } from './pages/about/about';
-import { Stories } from "./pages/stories/stories";
-import { AddStory } from "./pages/add-story/add-story";
+import { Contact } from './pages/contact/contact';
+import { Stories } from './pages/stories/stories';
+import { AddStory } from './pages/add-story/add-story';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: Home,
-  },
-  {
-    path: 'home',
-    component: Home,
-  },
-  {
-    path: 'about',
-    component: About,
-  },
-  {
-    path: 'contact',
-    component: About,
-  },
-  {
-    path: 'stories',
-    component: Stories,
-  },
-  {
-    path: 'add-story',
-    component: AddStory,
-  }
+  { path: '', component: Home },
+
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact },
+  { path: 'stories', component: Stories },
+
+  // Buổi 4
+  { path: 'add-story', component: AddStory },
 ];
--- src/app/pages/add-story/add-story.ts
+++ src/app/pages/add-story/add-story.ts
@@ -1,23 +1,90 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+
 @Component({
   selector: 'app-add-story',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './add-story.html',
   styleUrl: './add-story.css',
 })
 export class AddStory {
-  addForm: FormGroup;
+  // Bài 1
+  productForm: FormGroup;
+
+  // Bài 2
+  registerForm: FormGroup;
+
+  // Bài 3
+  loginForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.addForm = this.fb.group({
-      title: "",
-      author: "",
-      views: 0,
+    // Bài 1
+    this.productForm = this.fb.group({
+      name: ['', Validators.required],
+      price: ['', Validators.min(1)],
+      category: [''],
+    });
+
+    // Bài 2
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+
+    // Bài 3
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
-  submitForm() {
-    console.log(this.addForm.value);
+  // Product
+  get name() {
+    return this.productForm.get('name');
+  }
+
+  get price() {
+    return this.productForm.get('price');
+  }
+
+  // Register
+  get username() {
+    return this.registerForm.get('username');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  // Login
+  get loginUsername() {
+    return this.loginForm.get('username');
+  }
+
+  get loginPassword() {
+    return this.loginForm.get('password');
+  }
+
+  submitProduct() {
+    console.log(this.productForm.value);
+  }
+
+  submitRegister() {
+    console.log(this.registerForm.value);
+  }
+
+  submitLogin() {
+    console.log(this.loginForm.value);
   }
 }
